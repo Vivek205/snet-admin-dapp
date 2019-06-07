dfb4b83b7bbb00ec50b65cce3a3c8d55388effce
Vivek205 <n.vin95@gmail.com>
2019-06-07 18:13:22 +0530

    linted SectionMenu tsx

--- src/pages/operator/SectionMenu.tsx
+++ src/pages/operator/SectionMenu.tsx
@@ -1,35 +1,40 @@
-import React, { Component } from 'react';
-import { Menu } from './';
+import React, { ReactElement, ReactNode } from "react";
 
 // MaterialUI Imports
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import StyledSection from './StyledSection';
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import StyledSection from "./StyledSection";
 
 interface IProps {
-    classes: any;
-    sections: string[];
-    handleSectionChange(activeSection: string): void;
+  classes: any;
+  sections: string[];
+  handleSectionChange(activeSection: string): void;
 }
 
-const SectionMenu: React.FC<IProps> = (props) => {
-    const { classes, sections, handleSectionChange } = props;
-    return (
-        <Drawer
-            className={classes.drawer}
-            variant="permanent"
-            classes={{
-                paper: classes.drawerPaper,
-            }}
-        >
-            <div className={classes.toolbar} />
-            <List>
-                {sections.map((section: string, index: number) => (
-                    <StyledSection key={section} section={section} handleSectionChange={handleSectionChange} />
-                ))}
-            </List>
-        </Drawer>
-    );
+const SectionMenu: React.FC<IProps> = (props): ReactElement => {
+  const { classes, sections, handleSectionChange } = props;
+  return (
+    <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      classes={{
+        paper: classes.drawerPaper
+      }}
+    >
+      <div className={classes.toolbar} />
+      <List>
+        {sections.map(
+          (section: string, index: number): ReactNode => (
+            <StyledSection
+              key={section}
+              section={section}
+              handleSectionChange={handleSectionChange}
+            />
+          )
+        )}
+      </List>
+    </Drawer>
+  );
 };
 
 export default SectionMenu;
