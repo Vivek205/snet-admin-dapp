df9b73a5763304a0a791d1e0171cbf388879e514
Vivek205 <n.vin95@gmail.com>
2019-06-07 18:22:50 +0530

    linted MaterialUI Theme ts

--- src/assets/css/MaterialUITheme.ts
+++ src/assets/css/MaterialUITheme.ts
@@ -1,23 +1,15 @@
-import { createMuiTheme, Theme } from '@material-ui/core/styles';
-import blue from '@material-ui/core/colors/blue';
-import pink from '@material-ui/core/colors/pink';
+import { createMuiTheme, Theme } from "@material-ui/core/styles";
 
-let primaryMain = '#4f79f8';
-let secondaryMain = '#c5c5c5'
-// let secondaryMain = '#d7516c';
-let y = '#857af6';
-let z = '#6771f6';
+let primaryMain = "#4f79f8";
+let secondaryMain = "#c5c5c5";
 
 export const theme: Theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: primaryMain
-        },
-        secondary: {
-            main: secondaryMain
-        }
+  palette: {
+    primary: {
+      main: primaryMain
+    },
+    secondary: {
+      main: secondaryMain
     }
+  }
 });
-
-
-
