0bf8d56afc26b420aad90ed146d1c44c22150b7c
Vivek205 <n.vin95@gmail.com>
2019-06-07 18:03:58 +0530

    linted BooleanInput.tsx

--- src/pages/operator/BooleanInput.tsx
+++ src/pages/operator/BooleanInput.tsx
@@ -1,70 +1,87 @@
-import React, { Fragment } from 'react';
-import { anyObject } from '../../typeScript/interfaces';
-import { ICONS } from './icons';
+import React, { Fragment, ReactElement } from "react";
+import { anyObject } from "../../typeScript/interfaces";
+import { ICONS } from "./icons";
 
 // MaterialUI Imports
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
-import { withStyles } from '@material-ui/core/styles';
-import Tooltip from '@material-ui/core/Tooltip';
-import Icon from '@material-ui/core/Icon';
-import Zoom from '@material-ui/core/Zoom';
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+import { withStyles } from "@material-ui/core/styles";
+import Tooltip from "@material-ui/core/Tooltip";
+import Icon from "@material-ui/core/Icon";
+import Zoom from "@material-ui/core/Zoom";
 
 const styles = {
-    label: {
-        fontSize: '1.5rem',
-        marginRight: 15,
-    },
-    tooltip: {
-        fontSize: '1.5rem',
-    },
-    icon: {
-        verticalAlign: 'middle',
-        marginLeft: 5
-    }
-}
+  label: {
+    fontSize: "1.5rem",
+    marginRight: 15
+  },
+  tooltip: {
+    fontSize: "1.5rem"
+  },
+  icon: {
+    verticalAlign: "middle",
+    marginLeft: 5
+  }
+};
 
 interface IProps {
-    classes: anyObject;
-    checked: boolean;
-    label: string;
-    description: string;
-    handleChange(e: React.ChangeEvent<HTMLInputElement>, label: string): void;
-    disabled?: boolean;
+  classes: anyObject;
+  checked: boolean;
+  label: string;
+  description: string;
+  handleChange(e: React.ChangeEvent<HTMLInputElement>, label: string): void;
+  disabled?: boolean;
 }
 
-const BooleanInput: React.FC<IProps> = (props) => {
-    const { checked, label, handleChange, classes, description, disabled } = props;
-    console.log('boolean input', label, '==', checked);
-    return (
-        <FormControlLabel
-            labelPlacement="start"
-            control={
-                <Switch
-                    checked={checked}
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e, label)}
-                    value="checkedA"
-                    disabled={disabled}
-                />
-            }
-            label={<Fragment>
-                {label}
-                <Tooltip title={description}
-                    interactive TransitionComponent={Zoom}
-                    classes={{ tooltip: classes.tooltip }}>
-                    <Icon className={ICONS.tooltipInfo} classes={{ root: classes.icon }} fontSize='small' />
-                </Tooltip>
-                &nbsp;:
-            </Fragment>}
-            classes={{
-                label: classes.label
-            }}
+const BooleanInput: React.FC<IProps> = (props): ReactElement => {
+  const {
+    checked,
+    label,
+    handleChange,
+    classes,
+    description,
+    disabled
+  } = props;
+  return (
+    <FormControlLabel
+      labelPlacement="start"
+      control={
+        <Switch
+          checked={checked}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+            handleChange(e, label)
+          }
+          value="checkedA"
+          disabled={disabled}
         />
-    );
+      }
+      label={
+        <Fragment>
+          {label}
+          <Tooltip
+            title={description}
+            interactive
+            TransitionComponent={Zoom}
+            classes={{ tooltip: classes.tooltip }}
+          >
+            <Icon
+              className={ICONS.tooltipInfo}
+              classes={{ root: classes.icon }}
+              fontSize="small"
+            />
+          </Tooltip>
+          &nbsp;:
+        </Fragment>
+      }
+      classes={{
+        label: classes.label
+      }}
+    />
+  );
 };
 
 BooleanInput.defaultProps = {
-    disabled: false
-}
+  disabled: false
+};
 
 export default withStyles(styles)(BooleanInput);
