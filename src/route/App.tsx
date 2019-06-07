03822d061134c9a5cc5fec754bb78e2e7100788b
Vivek205 <n.vin95@gmail.com>
2019-06-07 17:36:58 +0530

    linted App.tsx

--- src/route/App.tsx
+++ src/route/App.tsx
@@ -1,47 +1,68 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, ReactNode } from "react";
+import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import asyncComponent from '../utilities/AsyncComponent';
-import BlockchainHelper from '../utilities/BlockChainHelper';
+import asyncComponent from "../utilities/AsyncComponent";
+import BlockchainHelper from "../utilities/BlockChainHelper";
 
 // MaterialUI Imports
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import { theme } from '../assets/css/MaterialUITheme';
+import { MuiThemeProvider } from "@material-ui/core/styles";
+import { theme } from "../assets/css/MaterialUITheme";
 
-const AsyncHome = asyncComponent(() => import('../pages/home/Home'));
-const AsyncPageNotFound = asyncComponent(() => import('../pages/PageNotFound'));
+//Code Splitting
+const AsyncHome = asyncComponent(
+  (): Promise<any> => import("../pages/home/Home")
+);
+const AsyncPageNotFound = asyncComponent(
+  (): Promise<any> => import("../pages/PageNotFound")
+);
 
 interface IState {
   web3Initialized: boolean;
 }
 
-class App extends Component<{}, IState>{
-  network: any = undefined;
+class App extends Component<{}, IState> {
+  private network: any = undefined;
 
-  state: IState = {
+  public state: IState = {
     web3Initialized: false
-  }
+  };
 
-  componentDidMount = (): void => {
-    window.addEventListener('load', () => this.handleWindowLoad());
-  }
+  public componentDidMount = (): void => {
+    window.addEventListener("load", (): void => this.handleWindowLoad());
+  };
 
-  handleWindowLoad = (): void => {
-    if (typeof window.web3 !== 'undefined') {
+  private handleWindowLoad = (): void => {
+    if (typeof window.web3 !== "undefined") {
       this.network = new BlockchainHelper(window.web3);
-      this.network.initialize().then((value: boolean) => {
-        this.setState({ web3Initialized: value });
-      }).catch(console.log);
+      this.network
+        .initialize()
+        .then((value: boolean): void => {
+          this.setState({ web3Initialized: value });
+        })
+        .catch((): void => {
+          // eslint-disable-next-line no-console
+          console.log("web3 not loaded. try reloading the page");
+        });
     }
-  }
+  };
 
-  render() {
+  public render(): ReactNode {
     return (
       <MuiThemeProvider theme={theme}>
-        <div className="App" >
+        <div className="App">
           <Router>
             <Switch>
-              <Route exact path='/' render={(props) => <AsyncHome {...props} web3Initialized={this.state.web3Initialized} network={this.network} />} />
+              <Route
+                exact
+                path="/"
+                render={(props: any): ReactNode => (
+                  <AsyncHome
+                    {...props}
+                    web3Initialized={this.state.web3Initialized}
+                    network={this.network}
+                  />
+                )}
+              />
               <Route component={AsyncPageNotFound} />
             </Switch>
           </Router>
