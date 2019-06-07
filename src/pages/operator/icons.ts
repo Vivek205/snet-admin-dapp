8c80502eac2f613fe4a38a140c0be37da4a4e3dd
Vivek205 <n.vin95@gmail.com>
2019-06-07 18:04:41 +0530

    linted icons.ts

--- src/pages/operator/icons.ts
+++ src/pages/operator/icons.ts
@@ -1,24 +1,24 @@
 export const ICONS: ICONS = {
-    'ratelimit': 'fas fa-greater-than-equal',
-    'general': 'fas fa-sliders-h',
-    'logging': 'fas fa-book',
-    'certificate': 'fas fa-suitcase',
-    'blockchain': 'fab fa-bitcoin',
-    'default': 'fab fa-intercom',
-    'tooltipInfo': 'fas fa-info-circle',
-    'start':'fas fa-play-circle',
-    'stop':'fas fa-stop-circle'
-}
+  ratelimit: "fas fa-greater-than-equal",
+  general: "fas fa-sliders-h",
+  logging: "fas fa-book",
+  certificate: "fas fa-suitcase",
+  blockchain: "fab fa-bitcoin",
+  default: "fab fa-intercom",
+  tooltipInfo: "fas fa-info-circle",
+  start: "fas fa-play-circle",
+  stop: "fas fa-stop-circle"
+};
 
 interface ICONS {
-    'ratelimit': string;
-    'general': string;
-    'logging': string;
-    'certificate': string;
-    'blockchain': string;
-    'default': string;
-    'tooltipInfo': string;
-    'start':string;
-    'stop':string;
-    [key:string]:string;
+  ratelimit: string;
+  general: string;
+  logging: string;
+  certificate: string;
+  blockchain: string;
+  default: string;
+  tooltipInfo: string;
+  start: string;
+  stop: string;
+  [key: string]: string;
 }
