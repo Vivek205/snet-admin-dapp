2b6465cd544baf1900a9bcfef7549122736b6058
Vivek205 <n.vin95@gmail.com>
2019-06-07 18:15:54 +0530

    linted PageNotFound tsx

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { ReactElement } from "react";
 
-const PageNotFound:React.FC = () => {
-    return (
-        <div>
-            <h1>! Page Not Found</h1>
-        </div>
-    );
+const PageNotFound: React.FC = (): ReactElement => {
+  return (
+    <div>
+      <h1>! Page Not Found</h1>
+    </div>
+  );
 };
 
 export default PageNotFound;
