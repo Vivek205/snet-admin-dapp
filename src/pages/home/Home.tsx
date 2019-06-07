7292bb6e98e27ba76e035216e16fca3d7d57427d
Vivek205 <n.vin95@gmail.com>
2019-06-07 18:03:12 +0530

    linted Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import ConnectWallet from '../connectWallet/ConnectWallet';
-import Operator from '../operator';
+import React, { Component, ReactNode } from "react";
+import ConnectWallet from "../connectWallet/ConnectWallet";
+import Operator from "../operator";
 
 interface IProps {
-	web3Initialized: boolean;
-	network: any;
+  web3Initialized: boolean;
+  network: any;
 }
 
 class Home extends Component<IProps> {
-
-	render() {
-		return (
-			<React.Fragment>
-				{this.props.web3Initialized ? <Operator network={this.props.network} /> : <ConnectWallet />}
-			</React.Fragment>
-		)
-	}
+  public render(): ReactNode {
+    return (
+      <React.Fragment>
+        {this.props.web3Initialized ? (
+          <Operator network={this.props.network} />
+        ) : (
+          <ConnectWallet />
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
 export default Home;
